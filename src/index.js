f7908e2ec4a8a75fb9f5c4059c7a2caae5685b0a
bornkes <born-kes@o2.pl>
2020-11-16 17:32:56 +0100

    add serviceWorker
    #install
    npm i
    
    #run
    npm run start
    
    #build
    npm run build

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render( <div>
+    <h1>Hello Word!!</h1>
+    <p> whot now?</p>
+</div> , document.getElementById('root') );
+
+
+// serviceWorker.unregister(); // Disable to work offline. This can make it difficult to remove errors
+// serviceWorker.register(); //The application will work without internet, but you cannot debug
