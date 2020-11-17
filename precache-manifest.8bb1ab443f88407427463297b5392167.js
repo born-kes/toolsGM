5adb58d7ea69f51f5e8c55ceced7284f35afee0a
born-kes <born-kes@o2.pl>
2020-11-17 16:00:40 +0100

    Updates

---
+++ precache-manifest.8bb1ab443f88407427463297b5392167.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "375d38ac6540b335a22655578d7b5ec9",
+    "url": "/toolsGM/index.html"
+  },
+  {
+    "revision": "6240181b2004142e77ae",
+    "url": "/toolsGM/static/css/2.a818b104.chunk.css"
+  },
+  {
+    "revision": "923386fe11e566e48c29",
+    "url": "/toolsGM/static/css/main.ba7c61e1.chunk.css"
+  },
+  {
+    "revision": "6240181b2004142e77ae",
+    "url": "/toolsGM/static/js/2.8d8a4501.chunk.js"
+  },
+  {
+    "revision": "4bf00ea42f5dad8ab33d5f32afec9baf",
+    "url": "/toolsGM/static/js/2.8d8a4501.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "923386fe11e566e48c29",
+    "url": "/toolsGM/static/js/main.85857a79.chunk.js"
+  },
+  {
+    "revision": "28b93340b0f079c8bd39",
+    "url": "/toolsGM/static/js/runtime-main.03676402.js"
+  }
+]);
